de75027b07ba97bb72bcc4fcab1ae04ec89f3f40
Ishuklaji <ishuklaji100@gmail.com>
2023-09-12 02:10:22 +0530

    working

--- server.js
+++ server.js
@@ -1,5 +1,25 @@
 const express = require('express')
+const colors = require('colors')
+const dotenv = require('dotenv')
 
+// configure env
+dotenv.config()
 
+// rest object
 const app = express()
 
+// rest api
+app.get('/', (req, res) => {
+    res.send(
+        "<h1>Welcome to Ecommo App</h1>"
+    )
+})
+
+// PORT
+const PORT = process.env.PORT || 8080
+
+
+// run listen
+app.listen(PORT, () => {
+    console.log(`Server is running on ${process.env.DEV_MODE} mode on port http://localhost:${PORT}`.cyan)
+})
