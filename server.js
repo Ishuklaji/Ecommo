1d644fdd8fcb82fc03398f136a44adfdca4de1fc
Ishuklaji <ishuklaji100@gmail.com>
2023-10-18 00:56:40 +0530

    working

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const app = express()
 // middlewares
 app.use(cors());
 app.use(express.json())
-app.use(morgan("dev")) // log requests to the console (only in development)
+app.use(morgan("dev")) // logs requests to the console (only in development)
 
 // routes
 app.use('/api/v1/auth',authRoutes)
