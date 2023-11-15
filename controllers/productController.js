2afbe49efdd2d3d420272b1d6186da2295d3e16e
Ishuklaji <ishuklaji100@gmail.com>
2023-11-16 00:55:11 +0530

    working on category react

--- controllers/productController.js
+++ controllers/productController.js
@@ -171,7 +171,7 @@ export const updateProductController = async (req, res) => {
         res.status(500).send({
             success: false,
             error,
-            message: "Error in Updte product",
+            message: "Error in Updating product",
         });
     }
 };
